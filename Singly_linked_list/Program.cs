1e7e1bdde5ee59a654354960b5ae67cef92e14b7
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-09 17:08:11 +0700

    add class list, and addNode method for adding anode into a list

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -13,6 +13,59 @@ namespace Singly_linked_list
         public Node next;
     }
 
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+
+        public void addNode()/*Method untuk menamabahkan sebuah Node kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs) /*Node ditambahkan sebagai node pertama*/
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+
+            }
+
+            /*Menemukan lokasi node baru didalam list*/
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while((current != null) && (nim >= current.noMhs))
+            {
+                if(nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan");
+                    return ;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru akan ditempatkan diantara previous dan current*/
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
